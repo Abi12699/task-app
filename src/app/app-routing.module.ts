7c17f937dd4f4d1cf42722421c0759ea8db554c7
Abishek Singh <arjun@Arjuns-MacBook-Air.local>
2025-05-09 10:40:13 +0530

    task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MyWebsiteComponent } from './my-website/my-website.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { CircleComponent } from './circle/circle.component';
+import { BMIComponent } from './bmi/bmi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',component:MyWebsiteComponent},
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'home',component:HomeComponent
+  }] },
+
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'Welcome',component:WelcomeComponent
+  }] },
+
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'Data Binding',component:DataBindingComponent
+  }] },
+
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'Calculator',component:CalculateComponent
+  }] },
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'Rectangle',component:RectangleComponent
+  }] },
+
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'Circle',component:CircleComponent
+  }] },
+
+  {path:'mywebsite',component:MyWebsiteComponent,children:[{
+    path:'BMI',component:BMIComponent
+  }] },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MyWebsiteComponent } from './my-website/my-website.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { CircleComponent } from './circle/circle.component';
+import { BMIComponent } from './bmi/bmi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyWebsiteComponent,
+    WelcomeComponent,
+    DataBindingComponent,
+    CalculateComponent,
+    RectangleComponent,
+    CircleComponent,
+    BMIComponent
   ],
   imports: [
     BrowserModule,
